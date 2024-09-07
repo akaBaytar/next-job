716fe220bcc4ea2f984a2eb2f79d0300eff7cef5
Burak Bilgili <burak.bilgili@icloud.com>
2024-09-07 09:28:14 +0300

    nav links

---
+++ utils/links.tsx
@@ -0,0 +1,13 @@
+import { AreaChart, Layers, AppWindow } from 'lucide-react';
+
+type NavLink = {
+  href: string;
+  label: string;
+  icon: React.ReactNode;
+};
+
+export const links: NavLink[] = [
+  { href: '/add-job', label: 'Add Job', icon: <Layers /> },
+  { href: '/jobs', label: 'Jobs', icon: <AppWindow /> },
+  { href: '/stats', label: 'Stats', icon: <AreaChart /> },
+];
